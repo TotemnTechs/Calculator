43bdb6c2b128ec3ce68c367650815150162b5eaa
Totenem <yganamichaelashley@gmail.com>
2025-09-28 02:53:11 +0800

    init

---
+++ index.py
@@ -0,0 +1,21 @@
+while True:
+    print("=============MENU=============")
+    print("[1] Add\n[2] Substract\n[3] Multiply\n[4] Divide")
+    choice = input("Enter Your Choice: ")
+    
+    match choice:
+        case "1":
+            num1 = int(input("Enter First Number: "))
+            num2 = int(input("Enter Second Number: "))
+             #klfjdkjfdksjf
+            results = num1 + num2
+            print(f"Results: {results}")
+            break
+        case "2":
+            ...
+        case "3":
+            ...
+        case "4":
+            ...
+        case _:
+            print("Invalid Code Menu")
